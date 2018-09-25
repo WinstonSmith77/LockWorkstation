243d4ce22fec503ad9255287498a5309dd0859f1
WinstonSmith77 <matze_henning@gmx.de>
2018-09-25 18:40:32 +0200

    Erste Version

--- LockWorkstation/Program.cs
+++ LockWorkstation/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LockWorkstation
 {
+
     class Program
     {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool LockWorkStation();
+
         static void Main(string[] args)
         {
+            if (!LockWorkStation())
+                throw new Win32Exception(Marshal.GetLastWin32Error()); // or any other thing
         }
     }
 }
