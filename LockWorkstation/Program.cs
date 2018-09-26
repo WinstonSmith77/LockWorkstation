a6c1009b70c11c923dd7dfbe1a64ee9b0a31369a
TECH2GO\mhenning <matze_henning@gmx.de>
2018-09-26 13:40:14 +0200

    Verbessert

--- LockWorkstation/Program.cs
+++ LockWorkstation/Program.cs
@@ -5,19 +5,38 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace LockWorkstation
 {
 
     class Program
     {
+        private const string Sperren = "Sperren?";
+
         [DllImport("user32.dll", SetLastError = true)]
         static extern bool LockWorkStation();
 
-        static void Main(string[] args)
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        static void Main()
         {
-            if (!LockWorkStation())
-                throw new Win32Exception(Marshal.GetLastWin32Error()); // or any other thing
+            var handle = GetConsoleWindow();
+
+            // Hide
+            ShowWindow(handle, SW_HIDE);
+
+            if (MessageBox.Show(Sperren, Sperren, MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                LockWorkStation();
+            }
         }
     }
 }
