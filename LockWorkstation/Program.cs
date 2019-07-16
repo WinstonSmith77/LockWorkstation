63c200f3561fafc438e1e1e0d2a28e623cce1d22
MatthiasHenning <m.henning@tech2go.de>
2019-07-16 14:22:42 +0200

    Keine UI mehr

--- LockWorkstation/Program.cs
+++ LockWorkstation/Program.cs
@@ -12,31 +12,12 @@ namespace LockWorkstation
 
     class Program
     {
-        private const string Sperren = "Sperren?";
-
         [DllImport("user32.dll", SetLastError = true)]
         static extern bool LockWorkStation();
 
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
-
-        [DllImport("user32.dll")]
-        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        const int SW_HIDE = 0;
-        const int SW_SHOW = 5;
-
         static void Main()
         {
-            var handle = GetConsoleWindow();
-
-            // Hide
-            ShowWindow(handle, SW_HIDE);
-
-            if (MessageBox.Show(Sperren, Sperren, MessageBoxButtons.YesNo) == DialogResult.Yes)
-            {
-                LockWorkStation();
-            }
+           LockWorkStation();
         }
     }
 }
